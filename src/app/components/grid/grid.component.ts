97b81d9f3c62c524e4f603bbc712e0f0460eba8f
victorgvargas <vvargas2013@gmail.com>
2024-01-03 09:48:01 +0000

    first

---
+++ src/app/components/grid/grid.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'spf-grid',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.scss']
+})
+export class GridComponent {
+
+}
---
+++ src/env/environment.development.ts
@@ -0,0 +1,4 @@
+export const ENVIRONMENT = {
+  access_token:
+    'BQB5V0ZF1Xzavg375Xj7FR1NADhG3QeZQOrWI6ww0DyGpFyW_W47J6Ua3CaXSYzOVmfP2GbLP6wEwe2Llc3Zs2EqbOw6gMLtdUQc-DGgTTeIiaSlvoI',
+};
